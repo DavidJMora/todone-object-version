a9662e419a5b2eb3f6bd7cc2fea3f86876829853
David Mora <David.mora@codeimmersives.com>
2019-03-20 11:48:53 -0400

    rearranged the addtodo function to allow for the error code to only show during an attempted duplication and show no error code when trying to add empty string

--- main.js
+++ main.js
@@ -52,10 +52,11 @@ function addTodo(event) {
     // Put our new element on the list part of our page!
     const addingToOl = document.querySelector('#todo-list');
     addingToOl.appendChild(newLi);
-    } else {
+    }else if(todoUserInput === '') {
+        document.querySelector('#error-message').innerText = '';
+    }else if(todos.includes(todoUserInput) === true) {
         document.querySelector('#error-message').innerText = "Please check to make sure todo list doesn't already have the item you are trying to enter.";
     }
-
     // Clear the input field of all text.
     document.querySelector('#new-todo').value = '';
 }
